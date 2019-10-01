bb236c1e32164aa4fc5bbf0b1cf7e376a275e514
Mario Silla Olmos <silolm.mail@gmail.com>
2019-10-01 19:43:05 +0200

    asda

---
+++ PrimeraEvaluacion/pruebas/validador.js
@@ -0,0 +1,44 @@
+var guardar;
+
+function validarNombre(Nombre) {
+    if (/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/.test(Nombre.value)) {
+        console.log("true");
+    } else {
+        Nombre.style.backgroundColor = "red";
+    }
+}
+
+function validarCorreo(Correo) {
+    if (/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(Correo.value)) {
+        console.log("true");
+    } else {
+        Correo.style.backgroundColor = "red";
+    }
+}
+
+function validarDNI(DNI) {
+    if (/^[0-9]{8,8}[A-Za-z]$/.test(DNI.value)) {
+        console.log("true");
+    } else {
+        DNI.style.backgroundColor = "red";
+    }
+}
+
+function validarContrasenya(Contrasenya) {
+    if (/^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/.test(Contrasenya.value)) {
+        console.log("true");
+    } else {
+        Contrasenya.style.backgroundColor = "red";
+    }
+
+    Contrasenya = guardar;
+}
+
+function repetircontrasenya(Contrasenya) {
+    if (guardar == Contrasenya) {
+        console.log("true");
+    } else {
+        Contrasenya.style.backgroundColor = "red";
+    }
+}
+
