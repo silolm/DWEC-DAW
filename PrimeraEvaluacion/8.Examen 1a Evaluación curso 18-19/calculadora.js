4e4a36c7a5df26693670518164d8248653266afb
Mario Silla Olmos <silolm.mail@gmail.com>
2019-10-13 15:37:42 +0200

    calculadora

--- PrimeraEvaluacion/8.Examen 1a Evaluación curso 18-19/calculadora.js
+++ PrimeraEvaluacion/8.Examen 1a Evaluación curso 18-19/calculadora.js
@@ -1,3 +1,50 @@
-window.addEventListener('load', function () {
+let operador = false;
+let estaVacio = true;
 
+window.addEventListener('load', () => {
+
+    for (let boton of document.querySelectorAll(".operador")) {
+        boton.addEventListener('click', () => {
+            insertarOperador(boton.innerText.trim());
+        });
+    }
+    for (let boton of document.querySelectorAll(".numero")) {
+        boton.addEventListener('click', () => {
+            insertarNumero(boton.innerText.trim());
+        });
+    }
+
+    document.getElementById("borrar").addEventListener('click', eliminar);
 });
+
+function insertarOperador(caracter) {
+    if (!operador) {
+        operador = true;
+        document.getElementById('screen').value += caracter;
+    }
+
+}
+
+function insertarNumero(caracter) {
+    if (estaVacio) document.getElementById('screen').value = caracter;
+    else document.getElementById('screen').value += caracter;
+
+    operador = false;
+    estaVacio = false;
+}
+
+function eliminar(caracter) {
+    if (!estaVacio) {
+        let contenido = document.getElementById('screen').value;
+
+        if (contenido.length == 1) {
+            document.getElementById('screen').value = "0";
+            estaVacio = true;
+        } else document.getElementById('screen').value = contenido.substring(0, contenido.length - 1);
+    }
+}
+
+function esOperador(caracter) {
+     
+}
+
