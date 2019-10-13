0d02bfa104ee92a84a83c5cb13dbc518ddc27bee
Mario Silla Olmos <silolm.mail@gmail.com>
2019-10-13 17:40:31 +0200

    v2

--- PrimeraEvaluacion/8.Examen 1a Evaluación curso 18-19/calculadora.js
+++ PrimeraEvaluacion/8.Examen 1a Evaluación curso 18-19/calculadora.js
@@ -1,7 +1,11 @@
 let operador = false;
 let estaVacio = true;
+let esEntero = true;
+let esParentesis = true;
 
 window.addEventListener('load', () => {
+
+    // relacionar eventos del HTML con las funciones
 
     for (let boton of document.querySelectorAll(".operador")) {
         boton.addEventListener('click', () => {
@@ -14,6 +18,8 @@ window.addEventListener('load', () => {
         });
     }
 
+    document.getElementById("borradoTotal").addEventListener('click', borradoTotal);
+
     document.getElementById("borrar").addEventListener('click', eliminar);
 });
 
@@ -22,18 +28,29 @@ function insertarOperador(caracter) {
         operador = true;
         document.getElementById('screen').value += caracter;
     }
-
 }
 
 function insertarNumero(caracter) {
-    if (estaVacio) document.getElementById('screen').value = caracter;
-    else document.getElementById('screen').value += caracter;
+    if (caracter == "()") {
+        if (esParentesis) {
+            caracter = "(";
+            esParentesis = false;
+        } else {
+            caracter = ")";
+            esParentesis = true;
+        }
+    }
+
+    if (estaVacio && caracter !== ".") document.getElementById('screen').value = caracter;
+    else if (!(!esEntero && caracter == ".")) document.getElementById('screen').value += caracter;
+
+    if (estaVacio && caracter !== "0") estaVacio = false;
+    if (caracter == ".") esEntero = false;
 
     operador = false;
-    estaVacio = false;
 }
 
-function eliminar(caracter) {
+function eliminar() {
     if (!estaVacio) {
         let contenido = document.getElementById('screen').value;
 
@@ -41,10 +58,24 @@ function eliminar(caracter) {
             document.getElementById('screen').value = "0";
             estaVacio = true;
         } else document.getElementById('screen').value = contenido.substring(0, contenido.length - 1);
+
+        operador = !!esOperador(document.getElementById('screen').value.slice(-1));
+
+        if (!esEntero) esEntero = contenido.slice(-1) == ".";
     }
 }
 
+function borradoTotal() {
+    document.getElementById('screen').value = "0";
+    operador = false;
+    estaVacio = true;
+    esEntero = true;
+    esParentesis = true;
+}
+
 function esOperador(caracter) {
-     
+    let arrayDeOperadores = ['+', '-', 'x', '/', '%'];
+
+    return arrayDeOperadores.indexOf(caracter) !== -1;
 }
 
