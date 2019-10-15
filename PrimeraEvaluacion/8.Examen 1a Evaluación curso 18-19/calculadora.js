6d06621594b0d20f97007b59dcca3dde2d26958b
Mario Silla Olmos <silolm.mail@gmail.com>
2019-10-15 02:07:23 +0200

    finalizar calcu

--- PrimeraEvaluacion/8.Examen 1a Evaluación curso 18-19/calculadora.js
+++ PrimeraEvaluacion/8.Examen 1a Evaluación curso 18-19/calculadora.js
@@ -1,9 +1,9 @@
 // declaracion de variables
-
 let operador = false;
 let estaVacio = true;
 let esEntero = true;
 let esParentesis = true;
+let resultado = false;
 
 window.addEventListener('load', () => {
 
@@ -26,51 +26,70 @@ window.addEventListener('load', () => {
 
     document.getElementById("borradoTotal").addEventListener('click', borradoTotal);
     document.getElementById("borrar").addEventListener('click', eliminar);
+    document.getElementById("resultado").addEventListener('click', generarResultado);
 });
 
 function insertarOperador(caracter) {
     if (!operador) {
+        let screen = document.getElementById('screen');
+
         // añadimos a la screen el OPERADOR, puesto que el último dígito no es un operador.
-        document.getElementById('screen').value += caracter;
+        screen.value += caracter;
 
         // al añadir un OPERADOR cambiamos las variables de abajo para cumplir con las restricciones a la hora de escribir.
         operador = true;
         esEntero = true;
+        estaVacio = true;
+
+        screen.value += "0";
     }
 }
 
 function insertarNumero(caracter) {
-    if (caracter == "()") {
+    let screen = document.getElementById('screen');
+
+    if (resultado) borradoTotal();
+
+    if (caracter === "()") {
         if (esParentesis) {
-            caracter = "(";
+            caracter = "(0";
             esParentesis = false;
         } else {
+            if (estaVacio) return;
             caracter = ")";
             esParentesis = true;
         }
     }
 
-    if (estaVacio && caracter !== ".") document.getElementById('screen').value = caracter;
-    else if (!(!esEntero && caracter == ".")) document.getElementById('screen').value += caracter;
+    if (estaVacio && caracter !== ".") screen.value = screen.value.substring(0, screen.value.length - 1) + caracter;
+    else if (!(!esEntero && caracter === ".")) screen.value += caracter;
 
-    if (estaVacio && caracter !== "0") estaVacio = false;
-    if (caracter == ".") esEntero = false;
+    if (caracter === "(0") estaVacio = true;
+
+    if (estaVacio && (caracter !== "0" && caracter !== "." && caracter !== "(0")) estaVacio = false;
+    if (caracter === "." && esEntero) {
+        esEntero = false;
+        estaVacio = true;
+        screen.value += "0";
+    }
 
     operador = false;
 }
 
 function eliminar() {
-    if (!estaVacio) {
-        let contenido = document.getElementById('screen').value;
+    let screen = document.getElementById('screen');
 
-        if (contenido.length == 1) {
-            document.getElementById('screen').value = "0";
+    if (screen.value.length > 0) {
+        let contenido = screen.value;
+
+        if (contenido.length === 1) {
+            screen.value = "0";
             estaVacio = true;
-        } else document.getElementById('screen').value = contenido.substring(0, contenido.length - 1);
+        } else screen.value = contenido.substring(0, contenido.length - 1);
 
-        operador = !!esOperador(document.getElementById('screen').value.slice(-1));
-
-        if (!esEntero) esEntero = contenido.slice(-1) == ".";
+        operador = !!esOperador(screen.value.slice(-1));
+        if (!esEntero) esEntero = contenido.slice(-1) === ".";
+        esParentesis = contenido.slice(-1) !== ")";
     }
 }
 
@@ -80,6 +99,7 @@ function borradoTotal() {
     estaVacio = true;
     esEntero = true;
     esParentesis = true;
+    resultado = false;
 }
 
 function esOperador(caracter) {
@@ -88,3 +108,11 @@ function esOperador(caracter) {
     return arrayDeOperadores.indexOf(caracter) !== -1;
 }
 
+function generarResultado() {
+    if (esParentesis && !estaVacio && !operador) {
+        let screen = document.getElementById('screen');
+        screen.value = eval(screen.value.replace("x", "*"));
+        resultado = true;
+        estaVacio = true;
+    }
+}
